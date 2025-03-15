8b7957b2c83249cb58f3bea85a2b72cf026f663b
enkopavle <enkopavle@gmail.com>
2025-03-15 15:16:29 +0200

    main-screen: add header

--- src/features/main/index.tsx
+++ src/features/main/index.tsx
@@ -1,6 +1,9 @@
 import {useEffect} from "react"
 import {StyleSheet, View} from "react-native"
+import {SafeAreaView} from "react-native-safe-area-context"
 import {setStatusBarHidden} from "expo-status-bar"
+
+import {AppText} from "@/shared/components"
 
 import {colors} from "@/constants"
 
@@ -9,12 +12,25 @@ export const MainScreen = () => {
     setStatusBarHidden(false)
   }, [])
 
-  return <View style={styles.container}></View>
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <AppText font="notito700" size={20} color={"royalRed"}>
+          Library
+        </AppText>
+      </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.background
+    backgroundColor: colors.background,
+    paddingHorizontal: 16
+  },
+  header: {
+    paddingTop: 18,
+    paddingBottom: 8
   }
 })
