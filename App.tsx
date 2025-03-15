de72dd494779eeb34070e31766f42e2219f13253
enkopavle <enkopavle@gmail.com>
2025-03-15 11:29:23 +0200

    splash-screen: add expo-splash-screen

--- App.tsx
+++ App.tsx
@@ -1,4 +1,10 @@
 import {Navigation} from "@/navigation"
+import * as SplashScreen from "expo-splash-screen"
+
+SplashScreen.setOptions({
+  duration: 500,
+  fade: true
+})
 
 export default function App() {
   return <Navigation />
